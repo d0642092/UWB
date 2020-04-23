86c5bbb4994d6bcc7cbdd2b8600fe12c814f70cb
hongwen <hongwenwang7@gmail.com>
2020-04-23 20:16:40 +0800

    initial commit

--- UWB_Data_Collect/Sklearn_Degree.py
+++ UWB_Data_Collect/Sklearn_Degree.py
@@ -32,98 +32,105 @@ for i in tmp_index:
     train_data.pop(i)
     real_data.pop(i)
     area.pop(i)
-data = pd.concat([train_data, real_data, area],axis=1)
 
-train, validation = train_test_split(data, test_size=0.3, random_state=None)
+b=[]
+for i in range(0, len(train_data), 5):
+    b.append(train_data[i:i+5])
 
-X_train = train.loc[:, ["An0095","Area"]].values
-X_validation = validation.loc[:, ["An0095","Area"]].values
+print(b)
 
-X_train_d2 = PolynomialFeatures(deg).fit_transform(X_train)
-X_validation_d2 = PolynomialFeatures(deg).fit_transform(X_validation)
-# print("X_train with degree=2:")
-# print(X_train_d2)
-
-y_train = train.loc[:, "Real95"].values.reshape(-1, 1)
-y_validation = validation.loc[:, "Real95"].values.reshape(-1, 1)
-
-reg_d1 = LinearRegression()
-reg_d1.fit(X_train, y_train)
-y_hat_1 = reg_d1.predict(X_validation)
-
-mse_d1 = mean_squared_error(y_validation, y_hat_1)
-print("MSE with degree=1: {:.0f}".format(mse_d1))
-
-reg_d2 = LinearRegression()
-reg_d2.fit(X_train_d2, y_train)
-y_hat_2 = reg_d2.predict(X_validation_d2)
-print(X_validation)
-mse_d2 = mean_squared_error(y_validation, y_hat_2)
-print("MSE with degree=4: {:.0f}".format(mse_d2))
-
-
-# X_arr = np.linspace(data[["An0095","Area"]].min(), data[["An0095","Area"]].max()).reshape(-1, 1)
-# X_arr_d1 = np.linspace(data[["An0095""Area"]].min(), data[["An0095","Area"]].max()).reshape(-1, 1)
-# X_arr_d2 = PolynomialFeatures(deg).fit_transform(X_arr)
-X_arr = train.loc[:, "An0095"].values
-y_arr = train.loc[:, "Real95"].values
-X_arr_d1 = train.loc[:, ["An0095","Area"]].values
-X_arr_d2 = PolynomialFeatures(deg).fit_transform(train.loc[:, ["An0095","Area"]].values)
-y_arr_d1 = reg_d1.predict(X_arr_d1)
-y_arr_d2 = reg_d2.predict(X_arr_d2)
+# data = pd.concat([train_data, real_data, area], axis=1)
 #
-plt.scatter(train["An0095"], train["Real95"], s=5, c="b", label="Train")
-plt.scatter(validation["An0095"], validation["Real95"], s=5, c="r", label="Validation")
-plt.plot(X_arr, y_arr_d1, c="c", linewidth=3, label="d=1")
-# plt.plot(X_arr, y_arr_d2, c="g", linewidth=0.1, label="d=4")
-plt.xlabel("distance")
-plt.ylabel("real")
-plt.legend(loc="upper left")
-plt.show()
-
-test = PolynomialFeatures(deg).fit_transform([[245,22500]])
-print(reg_d2.predict(test))
-print(222)
-
-test = PolynomialFeatures(deg).fit_transform([[470,60000]])
-print(reg_d2.predict(test))
-print(442)
-
-# fig = plt.figure()
-# # plt.plot(train_data, real_data, 'r.', markersize=5)
-# plt.plot(X_arr, y_arr, 'r.', markersize=10)
-# # plt.plot(X_test, y_, 'g.-', markersize=10)
-# plt.plot(X_arr_d2, y_arr_d2, 'g.', markersize=5)
-# # plt.plot(X_train, lr.predict(X_test[:, np.newaxis]), 'b-')
-# # plt.gca().add_collection(lc)
-# plt.legend(('Data', 'Data_predict'), loc='lower right')
-# plt.title('Isotonic regression')
+# train, validation = train_test_split(data, test_size=0.3, random_state=None)
+#
+# X_train = train.loc[:, ["An0095","Area"]].values
+# X_validation = validation.loc[:, ["An0095","Area"]].values
+#
+# X_train_d2 = PolynomialFeatures(deg).fit_transform(X_train)
+# X_validation_d2 = PolynomialFeatures(deg).fit_transform(X_validation)
+# # print("X_train with degree=2:")
+# # print(X_train_d2)
+#
+# y_train = train.loc[:, "Real95"].values.reshape(-1, 1)
+# y_validation = validation.loc[:, "Real95"].values.reshape(-1, 1)
+#
+# reg_d1 = LinearRegression()
+# reg_d1.fit(X_train, y_train)
+# y_hat_1 = reg_d1.predict(X_validation)
+#
+# mse_d1 = mean_squared_error(y_validation, y_hat_1)
+# print("MSE with degree=1: {:.0f}".format(mse_d1))
+#
+# reg_d2 = LinearRegression()
+# reg_d2.fit(X_train_d2, y_train)
+# y_hat_2 = reg_d2.predict(X_validation_d2)
+# print(X_validation)
+# mse_d2 = mean_squared_error(y_validation, y_hat_2)
+# print("MSE with degree=4: {:.0f}".format(mse_d2))
+#
+#
+# # X_arr = np.linspace(data[["An0095","Area"]].min(), data[["An0095","Area"]].max()).reshape(-1, 1)
+# # X_arr_d1 = np.linspace(data[["An0095""Area"]].min(), data[["An0095","Area"]].max()).reshape(-1, 1)
+# # X_arr_d2 = PolynomialFeatures(deg).fit_transform(X_arr)
+# X_arr = train.loc[:, "An0095"].values
+# y_arr = train.loc[:, "Real95"].values
+# X_arr_d1 = train.loc[:, ["An0095","Area"]].values
+# X_arr_d2 = PolynomialFeatures(deg).fit_transform(train.loc[:, ["An0095","Area"]].values)
+# y_arr_d1 = reg_d1.predict(X_arr_d1)
+# y_arr_d2 = reg_d2.predict(X_arr_d2)
+# #
+# plt.scatter(train["An0095"], train["Real95"], s=5, c="b", label="Train")
+# plt.scatter(validation["An0095"], validation["Real95"], s=5, c="r", label="Validation")
+# plt.plot(X_arr, y_arr_d1, c="c", linewidth=3, label="d=1")
+# # plt.plot(X_arr, y_arr_d2, c="g", linewidth=0.1, label="d=4")
+# plt.xlabel("distance")
+# plt.ylabel("real")
+# plt.legend(loc="upper left")
 # plt.show()
 #
-
-
-
-
-
-# mse = mean_squared_error(y_validation, y_hat)
-
-# print("MSE of simple linear regression: {:.0f}".format(mse))
+# test = PolynomialFeatures(deg).fit_transform([[245,22500]])
+# print(reg_d2.predict(test))
+# print(222)
 #
-# print("Simple")
-# print([y_hat,y_validation,X_validation])
-
-
-
-
-
-
-
-# print("Multiple")
-# X_train_multiple = train.loc[:, ["An0095", "Area"]].values
-# X_validation_multiple = validation.loc[:, ["An0095", "Area"]].values
-# reg_multiple = LinearRegression()
-# reg_multiple.fit(X_train_multiple, y_train)
-# y_hat_multiple = reg_multiple.predict(X_validation_multiple)
-# mse_multiple = mean_squared_error(y_validation, y_hat_multiple)
-# print("MSE of multiple linear regression: {:.0f}".format(mse_multiple))
-# print([y_hat_multiple,y_validation,X_validation_multiple])
+# test = PolynomialFeatures(deg).fit_transform([[470,60000]])
+# print(reg_d2.predict(test))
+# print(442)
+#
+# # fig = plt.figure()
+# # # plt.plot(train_data, real_data, 'r.', markersize=5)
+# # plt.plot(X_arr, y_arr, 'r.', markersize=10)
+# # # plt.plot(X_test, y_, 'g.-', markersize=10)
+# # plt.plot(X_arr_d2, y_arr_d2, 'g.', markersize=5)
+# # # plt.plot(X_train, lr.predict(X_test[:, np.newaxis]), 'b-')
+# # # plt.gca().add_collection(lc)
+# # plt.legend(('Data', 'Data_predict'), loc='lower right')
+# # plt.title('Isotonic regression')
+# # plt.show()
+# #
+#
+#
+#
+#
+#
+# # mse = mean_squared_error(y_validation, y_hat)
+#
+# # print("MSE of simple linear regression: {:.0f}".format(mse))
+# #
+# # print("Simple")
+# # print([y_hat,y_validation,X_validation])
+#
+#
+#
+#
+#
+#
+#
+# # print("Multiple")
+# # X_train_multiple = train.loc[:, ["An0095", "Area"]].values
+# # X_validation_multiple = validation.loc[:, ["An0095", "Area"]].values
+# # reg_multiple = LinearRegression()
+# # reg_multiple.fit(X_train_multiple, y_train)
+# # y_hat_multiple = reg_multiple.predict(X_validation_multiple)
+# # mse_multiple = mean_squared_error(y_validation, y_hat_multiple)
+# # print("MSE of multiple linear regression: {:.0f}".format(mse_multiple))
+# # print([y_hat_multiple,y_validation,X_validation_multiple])
