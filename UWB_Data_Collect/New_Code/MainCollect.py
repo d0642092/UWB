17f89321b0d6eb5934e6c041d874ed6951820fb7
hongwen <hongwenwang7@gmail.com>
2020-05-03 22:53:17 +0800

    sleep

--- UWB_Data_Collect/New_Code/MainCollect.py
+++ UWB_Data_Collect/New_Code/MainCollect.py
@@ -1,5 +1,5 @@
 import time
-import threading
+import threading  # https://dotblogs.com.tw/yc421206/2011/01/04/20575
 from UWB_Data_Collect.New_Code.CatchData import catchData
 from UWB_Data_Collect.New_Code.WriteData import writerData
 from UWB_Data_Collect.New_Code.ControllCar import Forward
@@ -10,8 +10,8 @@ class ServerNotAlive(threading.Thread):
         self.flag = flag
     def run(self):
         while self.flag:
-            if not controlCar.flag:  # 會占用主程式 不能用~controlCar.flag
-                dataCatch.flag = False
+            if not controlCar.flag:  # 會占用主程式所以另用一個thread 不能用~controlCar.flag
+                dataCatch.flag = False  # 應該沒有意義到<有sleep>
                 print("server close")
                 self.flag = False
                 exit()
@@ -23,15 +23,17 @@ if __name__ == "__main__":
     total_distance = 100  # How long if the car run car_runTime second
     avg_V = total_distance / car_runTime  # The average velocity
 
-    dataCatch = catchData("CatchData", True)   # car 延遲兩秒 sleep(2)
-    # dataWrite = writerData("WriteData", 1, True, avg_V)
     controlCar = Forward("ControlCar", True)
     checkServer = ServerNotAlive(True)
+    dataCatch = catchData("CatchData", True)  # car 延遲兩秒 sleep(2)
+    dataWrite = writerData("WriteData", 1, True, avg_V)
 
     controlCar.start()
     checkServer.start()
+
+    time.sleep(2.2)
     dataCatch.start()
-    # dataWrite.start()
+    dataWrite.start()
 
 
     carStart = time.time()
@@ -52,6 +54,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         controlCar.flag = False
         dataCatch.flag = False
+        checkServer.flag = False
 
-    # dataWrite.undone = False
-    # dataWrite.join()
+    dataWrite.undone = False
+    dataWrite.join()
