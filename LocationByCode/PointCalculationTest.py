64f77c01769f5588f2c70e50210250b247734b0a
D641593 <D0641593@o365.fcu.edu.tw>
2020-06-19 22:08:50 +0800

    加入點測試案例

--- LocationByCode/PointCalculationTest.py
+++ LocationByCode/PointCalculationTest.py
@@ -38,15 +38,48 @@ class PCtest(unittest.TestCase):
             self.PC.get_group(2)
 
     def test_get_cal_array(self):
-        res = self.PC.get_cal_array(self.PC.get_group(4))
-        print(res)
-        ans = [[0,0]]*4
-        self.assertEqual(res,ans)
+        Dis = [70.7]*3
+        res = self.PC.get_cal_array(self.PC.get_group(3))
+        ans = [0,0]
+        for i in range(len(ans)):
+            self.assertAlmostEqual(res[0][i],ans[i],delta=2)
+
+    def test_get_cal_array1(self):
+        Dis = [100,100,223.6]
+        self.PC.set_dis(Dis)
+        res = self.PC.get_cal_array(self.PC.get_group(3))
+        ans = [100,0]
+        for i in range(len(ans)):
+            self.assertAlmostEqual(res[0][i],ans[i],delta=2)
+
+    def test_get_cal_array2(self):
+        Dis = [223.606,100,100]
+        self.PC.set_dis(Dis)
+        res = self.PC.get_cal_array(self.PC.get_group(3))
+        ans = [0,-100]
+        for i in range(len(ans)):
+            self.assertAlmostEqual(res[0][i],ans[i],delta=2)
+
+    def test_get_cal_array3(self):
+        Dis = [316.22,316.22,141.4213]
+        self.PC.set_dis(Dis)
+        res = self.PC.get_cal_array(self.PC.get_group(3))
+        ans = [-200,0]
+        for i in range(len(ans)):
+            self.assertAlmostEqual(res[0][i],ans[i],delta=2)
+
+    def test_get_cal_array4(self):
+        Dis = [111.8,180.26,180.26,111.8]
+        self.PC.set_dis(Dis)
+        res = self.PC.get_cal_array(self.PC.get_group(3))
+        ans = [0,50]
+        for i in range(len(ans)):
+            self.assertAlmostEqual(res[0][i],ans[i],delta=2)
+
     # def test_get_point(self):
     #     
     # def test_get_colse_point(self):
     #     
-
 if __name__ == "__main__":
         unittest.main()  # py .\PointCalculationTest.py -v         -->      use -v to get more information
 
